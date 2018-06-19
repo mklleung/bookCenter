f3da6b20ace465ef771255e159fb886d8732f0e6
mklleung <mkleung@brandeis.edu>
2018-06-19 14:50:32 -0400

    Added Google log in

--- app.js
+++ app.js
@@ -7,14 +7,26 @@ const
   path = require('path');
   cookieParser = require('cookie-parser');
   logger = require('morgan');
+  session = require("express-session"),
+  bodyParser = require("body-parser"),
+  User = require( './models/user' ),
+  flash = require('connect-flash')
 
   indexRouter = require('./routes/index');
   usersRouter = require('./routes/users');
   allBooksRouter = require('./routes/allBooks');
   departmentsRouter = require('./routes/departments');
   watchlistRouter = require('./routes/watchlist');
-  yourPageRouter = require('./routes/yourPage');
+  profileRouter = require('./routes/profile');
   //addBooksRouter = require('./routes/addBooks');
+
+  var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+   // here we set up authentication with passport
+   const passport = require('passport')
+   const configPassport = require('./config/passport')
+   configPassport(passport)
+
 
 var app = express();
 
@@ -30,10 +42,16 @@ db.once('open', function() {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//middleware to process the req object and make it more useful!
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({ secret: 'zzbbyanana' }));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -41,20 +59,78 @@ app.use('/users', usersRouter);
 app.use('/allBooks', allBooksRouter);
 app.use('/departments', departmentsRouter);
 app.use('/watchlist', watchlistRouter);
-app.use('/yourPage', yourPageRouter);
+app.use('/profile2',isLoggedIn, profileRouter);
 //app.use('/addBooks', addBooksRouter);
 
-app.get('/logins', logInController.getAllLogin );
-app.post('/saveLogin', logInController.saveLogin );
-app.post('/deleteLogin', logInController.deleteLogin );
+app.get('/loginerror', function(req,res){
+  res.render('loginerror',{})
+})
+
+app.get('/logins', function(req,res){
+  res.render('logins',{})
+})
+
+// we require them to be logged in to see their profile
+app.get('/profile', isLoggedIn, function(req, res) {
+      console.log(`req.user = ${req.user}`)
+        res.render('profile', {
+            user : req.user // get the user out of session and pass to template
+        });
+    });
+
+// route for logging out
+app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+
+
+    // =====================================
+    // GOOGLE ROUTES =======================
+    // =====================================
+    // send to google to do the authentication
+    // profile gets us their basic information including their name
+    // email gets their emails
+    app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+
+    // the callback after google has authenticated the user
+    app.get('/auth/google/callback',
+            passport.authenticate('google', {
+                    successRedirect : '/profile',
+                    failureRedirect : '/loginerror'
+            }));
+
+    app.get('/login/authorized',
+            passport.authenticate('google', {
+                    successRedirect : '/profile',
+                    failureRedirect : '/loginerror'
+            }));
+
+// route middleware to make sure a user is logged in
+function isLoggedIn(req, res, next) {
+    console.log("checking to see if they are authenticated!")
+    // if user is authenticated in the session, carry on
+    if (req.isAuthenticated()){
+      console.log("user has been Authenticated")
+      return next();
+    }
+
+    console.log("user has not been authenticated...")
+    // if they aren't redirect them to the home page
+    res.redirect('/logins');
+}
+
+/*app.get('/logins', isLoggedIn,logInController.getAllLogin );
+app.post('/saveLogin', isLoggedIn,logInController.saveLogin );
+app.post('/deleteLogin', isLoggedIn,logInController.deleteLogin );
 app.use('/logins', function(req, res, next) {
   console.log("in / controller")
   res.render('logins', { title: 'Book Center App' });
-});
-
-app.get('/addBooks', addBooksController.getAllBooks );
-app.post('/saveBook', addBooksController.saveBook );
-app.post('/deleteBook', addBooksController.deleteBook );
+});*/
+app.get('/allBooks',addBooksController.getAllBooks );
+app.get('/addBooks',addBooksController.getAllBooks );
+app.post('/saveBook',addBooksController.saveBook );
+app.post('/deleteBook',addBooksController.deleteBook );
 app.use('/addBooks', function(req, res, next) {
   console.log("in / controller")
   res.render('addBooks', { title: 'Book Center App' });
--- routes/profile.js
+++ routes/profile.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('yourPage', { title: 'Your Page' });
+  res.render('profile', { title: 'Profile' });
 });
 
 module.exports = router;
