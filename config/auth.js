ef56342c5ffae2114c4cc2283bd997894b9323b9
mklleung <mkleung@brandeis.edu>
2018-06-26 14:27:31 -0400

    new auth

--- config/auth.js
+++ config/auth.js
@@ -6,5 +6,9 @@ module.exports = {
         'clientSecret'  : 'KY8ZVEEm2rh7qf--mvi0Qwv6',
         'callbackURL'   : 'http://127.0.0.1:6300/login/authorized'
     }
-
+    'googleAuth' : {
+        'clientID': '669078234348-11r8igcfp3g6bsmr6kt294jm24osq4dt.apps.googleusercontent.com',
+        'clientSecret'  : 'KY8ZVEEm2rh7qf--mvi0Qwv6',
+        'callbackURL'   : 'http://leiner.cs-i.brandeis.edu:6300/'
+    }
 };
