55dbd5ee2eab49a09778e1585e1ec655a381ce51
mklleung <mkleung@brandeis.edu>
2018-06-26 14:17:22 -0400

    auth changes

--- config/auth.js
+++ config/auth.js
@@ -2,9 +2,9 @@ module.exports = {
 
 
     'googleAuth' : {
-        'clientID': '246096591118-ti33uv184e4m1bib9grgn8alm45btadb.apps.googleusercontent.com',
-        'clientSecret'  : 'iqgLqu6pXgLuHsZFq6nvxDX3',
-        'callbackURL'   : 'http://127.0.0.1:5000/login/authorized'
+        'clientID': '669078234348-11r8igcfp3g6bsmr6kt294jm24osq4dt.apps.googleusercontent.com',
+        'clientSecret'  : 'KY8ZVEEm2rh7qf--mvi0Qwv6',
+        'callbackURL'   : 'http://127.0.0.1:6300/login/authorized'
     }
 
 };
