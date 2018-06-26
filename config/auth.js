db6a60b576276a866a154ba4b7158aaf9aa0c687
mklleung <mkleung@brandeis.edu>
2018-06-26 15:08:27 -0400

    auth more2

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
     'googleAuth' : {
         'clientID': '669078234348-11r8igcfp3g6bsmr6kt294jm24osq4dt.apps.googleusercontent.com',
         'clientSecret'  : 'KY8ZVEEm2rh7qf--mvi0Qwv6',
-        'callbackURL'   : 'http://127.0.0.1:6300/login/authorized'
-        //'callbackURL'   : 'http://leiner.cs-i.brandeis.edu:6300/login/authorized'
+        //'callbackURL'   : 'http://127.0.0.1:6300/login/authorized'
+        'callbackURL'   : 'http://leiner.cs-i.brandeis.edu:6300/login/authorized'
     }
 };
