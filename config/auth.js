3336de2dd1c3fdc8253410fe87ffb1d0127d67fa
mklleung <mkleung@brandeis.edu>
2018-06-26 14:31:32 -0400

    new auth3

--- config/auth.js
+++ config/auth.js
@@ -5,6 +5,6 @@ module.exports = {
         'clientID': '669078234348-11r8igcfp3g6bsmr6kt294jm24osq4dt.apps.googleusercontent.com',
         'clientSecret'  : 'KY8ZVEEm2rh7qf--mvi0Qwv6',
         //'callbackURL'   : 'http://127.0.0.1:6300/login/authorized'
-        'callbackURL'   : 'http://leiner.cs-i.brandeis.edu:6300/'
+        'callbackURL'   : 'http://leiner.cs-i.brandeis.edu:6300/login/authorized'
     }
 };
